7ebd17673d1578d972324f33e0e4c9d081c24b6a
holywend <wenddarkknight@gmail.com>
2021-02-07 14:20:09 +0700

    Setting up config

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'wend.web.id',
+	'username' => 'wendwebi_bef',
+	'password' => 'borneoeducation',
+	'database' => 'wendwebi_',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
